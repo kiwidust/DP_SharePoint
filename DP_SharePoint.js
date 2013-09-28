a8e44cdaea7a240411dede42d97cb4b477157021
Jim Davis <jim@depressedpress.com>
2013-09-28 16:09:51 -0400

    Initial Commit

---
+++ DP_SharePoint.js
@@ -0,0 +1,628 @@
+/*  DepressedPress.com DP_SharePoint
+
+Author: Jim Davis, the Depressed Press of Boston
+Date: August 03, 2012
+Contact: webmaster@depressedpress.com
+Website: www.depressedpress.com
+
+Full documentation can be found at:
+http://www.depressedpress.com/
+
+DP_SharePoint abstracts common, simple tasks in SharePoint 2007 and 2010
+
+	- DP_Debug: Built-in Debugging requires DP_Debug (available from depressedpress.com).
+	- DP_DateExtensions: All DateTime-related method requires DP_DateExtensions (also available from the depressedpress.com)
+
+
+Copyright (c) 1996-2012, The Depressed Press (depressedpress.com)
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
++) Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer. 
+
++) Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution. 
+
++) Neither the name of the THE DEPRESSED PRESS (DEPRESSEDPRESS.COM) nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission. 
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+	// Manage other, optional, DP components
+	//
+
+	// Set up DP_Debug (prevents errors if DP_Debug is not present
+try {  DP_Debug.isEnabled()  } catch (e) {  DP_Debug = new Object(); DP_Debug.isEnabled = function() { return false }  };
+
+
+	// Create the Root DP_SharePoint object
+	//
+DP_SharePoint = {};
+
+
+
+	// Add/Edit Template field methods.
+	//
+
+	// DP_SharePoint.getControlRef Method - returns references to the "OK" and "Cancel" buttons on SharePoint forms
+	//
+DP_SharePoint.getControlRef = function() {
+
+		// Set an object to collect the output
+	var Output = {"Submit":[], "Cancel":[]};
+		// Get All input tags
+	var inputTags = document.getElementsByTagName("INPUT");
+		// Find the correct Tags
+	for (var cnt=0; cnt < inputTags.length; cnt++) {
+			// Find the buttons
+		if ( inputTags[cnt].type == "button" ) {
+			if ( inputTags[cnt].value == "OK" || inputTags[cnt].value == "Save" ) {
+				Output.Submit.push(inputTags[cnt]);
+			};
+			if ( inputTags[cnt].value == "Cancel" ) {
+				Output.Cancel.push(inputTags[cnt]);
+			};
+		};
+	};
+		// Return
+	return Output;
+
+};
+
+
+	// DP_SharePoint.getFieldRef Method
+	//
+DP_SharePoint.getFieldRef = function( Title ) {
+
+		// Get the type of form
+	var FormType = "";
+	if ( document.location.href.indexOf("EditForm.aspx") > -1 ) {
+		FormType = "Edit";
+	} else if ( document.location.href.indexOf("NewForm.aspx") > -1 ) {
+		FormType = "New";	
+	} else if ( document.location.href.indexOf("DispForm.aspx") > -1 ) {
+		FormType = "Display";	
+	};
+
+		// Set an object to collect the output
+	var Output = {};
+
+		// Get All h3 tags
+	var h3Tags = document.getElementsByTagName("H3");
+
+		// Find the correct h3
+	var FieldFound = false;
+	for (var cnt=0; cnt < h3Tags.length; cnt++) {
+		if ( h3Tags[cnt] && h3Tags[cnt].innerHTML.indexOf(Title) > -1 ) {
+			Output.LabelRef = h3Tags[cnt];
+			FieldFound = true;
+			break;
+		};
+	};
+		// Return null (failure) if the field wasn't found
+	if ( !FieldFound ) { return null };
+
+		// Get the containing TR
+	Output.RowRef = Output.LabelRef.parentNode.parentNode;
+
+		// Parse out the internal comments  to gather data
+	var CurComments = Output.RowRef.getElementsByTagName("TD")[1].firstChild.nodeValue.match(/"[^"]*"/g);
+	Output.Name = CurComments[0].substring(1, CurComments[0].length - 1);
+	Output.InternalName = CurComments[1].substring(1, CurComments[1].length - 1);
+	Output.Type = CurComments[2].substring(1, CurComments[2].length - 1);
+	Output.SubType = "";
+
+		// Gather References to the fields, according to FieldTypes (only for "Edit" and "New" forms)
+	if ( FormType == "Display" ) {
+			// Pull the contents of the field
+		Output.ElText = Output.RowRef.getElementsByTagName("TD")[1].innerHTML;
+			// Strip out the Comments
+		Output.ElText = Output.ElText.replace(/<!--(.*?)-->/gm, "");
+	} else if ( ( FormType == "Edit" ) || ( FormType == "New" ) ) {
+		switch( Output.Type ) {
+
+				// Simple INPUT tags
+			case "SPFieldText":
+			case "SPFieldNumber":
+			case "SPFieldCurrency":
+			case "SPFieldBoolean":
+
+				Output.ElRef = Output.RowRef.getElementsByTagName("INPUT")[0];
+
+			break;
+				// SELECT, RADIO and CHECKBOX tags
+			case "SPFieldChoice":
+			case "SPFieldMultiChoice":
+
+				Output.ElRef = {};
+				if ( Output.RowRef.getElementsByTagName("SELECT")[0] ) {
+					if ( Output.RowRef.getElementsByTagName("INPUT")[0] ) {
+						Output.ElRef.Select = Output.RowRef.getElementsByTagName("SELECT")[0];
+						Output.SubType = "SelectWithCustom";
+					} else {
+						Output.ElRef = Output.RowRef.getElementsByTagName("SELECT")[0];
+						Output.SubType = "Select";
+					};
+				};
+
+				if ( Output.RowRef.getElementsByTagName("INPUT")[0] ) {
+					var CurInputs = Output.RowRef.getElementsByTagName("INPUT");
+					Output.ElRef.Choices = [];
+					for (var cnt=0; cnt < CurInputs.length; cnt++) {
+						if ( CurInputs[Cnt].type != "text" ) {
+							Output.ElRef.Choices.push(CurInputs[Cnt]);						
+						};
+						if ( CurInputs[Cnt].type == "text" ) {
+							Output.ElRef.CustomChoice = CurInputs[Cnt - 1];
+							Output.ElRef.Custom = CurInputs[Cnt];
+						};
+					};			
+					if ( !Output.SubType ) {
+						if ( CurInputs[Cnt].type == "radio" ) {
+							if ( Output.ElRef.Custom ) {
+								Output.SubType = "RadioWithCustom";
+							} else {
+								Output.SubType = "Radio";
+							};
+						} else if ( CurInputs[Cnt].type == "checkbox" ) {
+							if ( Output.ElRef.Custom ) {
+								Output.SubType = "CheckboxWithCustom";
+							} else {
+								Output.SubType = "Checkbox";
+							};					
+						};		
+					};
+				};
+
+			break;
+				// Simple TEXTAREA tags
+			case "SPFieldNote":
+
+				var BaseTag = Output.RowRef.getElementsByTagName("TEXTAREA")[0];
+				Output.ElRef = {};
+				Output.ElRef.TextArea = BaseTag;
+				Output.ElRef.EditorDoc = RTE_GetEditorDocument(BaseTag.id);
+
+			break;
+				// Can be a SELECT or an INPUT
+			case "SPFieldLookup":
+
+				Output.ElRef = Output.RowRef.getElementsByTagName("SELECT")[0];
+				Output.SubType = "Select";
+				if ( !Output.ElRef ) {
+					Output.ElRef = Output.RowRef.getElementsByTagName("INPUT")[0];
+					Output.SubType = "Input";
+				};
+
+			break;
+				// A base hidden INPUT, two SELECT fields and two buttons
+			case "SPFieldLookupMulti":
+
+				var BaseTag = Output.RowRef.getElementsByTagName("INPUT")[0];
+				Output.ElRef = {};
+				Output.ElRef.Source = BaseTag;
+				Output.ElRef.Candidates = document.getElementById(BaseTag.id.replace("MultiLookupPicker", "SelectCandidate"));
+				Output.ElRef.Selected = document.getElementById(BaseTag.id.replace("MultiLookupPicker", "SelectResult"));
+				Output.ElRef.AddButton = document.getElementById(BaseTag.id.replace("MultiLookupPicker", "AddButton"));
+				Output.ElRef.RemoveButton = document.getElementById(BaseTag.id.replace("MultiLookupPicker", "RemoveButton"));
+
+			break;
+			case "SPFieldUser":
+			case "SPFieldUserMulti":
+
+				Output.ElRef = {};
+				Output.ElRef.Display = Output.RowRef.getElementsByTagName("TEXTAREA")[0];
+				Output.ElRef.InputData = Output.RowRef.getElementsByTagName("INPUT")[0];
+				Output.ElRef.SpanData = Output.RowRef.getElementsByTagName("INPUT")[1];
+				Output.ElRef.XMLData = Output.RowRef.getElementsByTagName("INPUT")[2];
+				Output.ElRef.CheckNamesLink = Output.RowRef.getElementsByTagName("A")[0];
+				Output.ElRef.BrowseNamesLink = Output.RowRef.getElementsByTagName("A")[1];
+			break;
+			case "SPFieldDateTime":
+
+				var BaseTag = Output.RowRef.getElementsByTagName("INPUT")[0];
+				Output.ElRef = {};
+				Output.ElRef.Date = BaseTag;
+				Output.ElRef.Hours = document.getElementById(BaseTag.id + "Hours");
+				Output.ElRef.Minutes = document.getElementById(BaseTag.id + "Minutes");
+				if ( Output.ElRef.Hours ) {
+					Output.SubType = "Date";
+				} else {
+					Output.SubType = "DateTime";
+				};
+			break;
+			case "SPFieldURL":
+
+				Output.ElRef = {};
+				Output.ElRef.URL = Output.RowRef.getElementsByTagName("INPUT")[0];
+				Output.ElRef.Label = Output.RowRef.getElementsByTagName("INPUT")[1];
+
+			break;
+
+		};
+	};
+	
+		// Return the output object
+	return Output;
+
+};
+
+
+	// DP_SharePoint.addFieldEvent Method
+	//
+DP_SharePoint.addFieldEvent = function( Field, Event, Handler ) {
+
+	if (Field.addEventListener) {
+		Field.addEventListener(Event, Handler, false);
+	} else if (Field.attachEvent) {
+		Field.attachEvent("on" + Event, Handler);
+	};
+
+};
+
+
+	// DP_SharePoint.removeFieldEvent Method
+	//
+DP_SharePoint.removeFieldEvent = function( Field, Event, Handler ) {
+
+	if (Field.removeEventListener) {
+		Field.removeEventListener(Event, Handler, false);
+	} else if (Field.detachEvent) {
+		Field.detachEvent("on" + Event, Handler);
+	};
+
+};
+
+	// DP_SharePoint.hideField Method
+DP_SharePoint.hideField = function( Field ) {
+
+		// Hide the field
+	Field.RowRef.style.display = "none";
+		// Return
+	return true;
+
+};
+
+	// DP_SharePoint.showField Method
+DP_SharePoint.showField = function( Field ) {
+
+		// Show the field
+	Field.RowRef.style.display = "inline";
+		// Return
+	return true;
+
+};
+
+
+
+	// Webservice Methods
+	//
+
+	// DP_SharePoint.callListService_Get Method
+	//
+DP_SharePoint.callListService_Get = function( Request, ServiceURL, ListName, ViewName, Query, QueryOptions, ViewFields ) {
+
+	if ( !Query ) {
+		Query = "";
+	} else {
+		Query = "<query>" + Query + "</query>";
+	};
+
+	if ( !QueryOptions ) {
+		QueryOptions = "";
+	} else {
+		QueryOptions = "<queryOptions>" + QueryOptions + "</queryOptions>";
+	};
+
+	if ( !ViewFields ) {
+		ViewFields = "";
+	} else {
+		ViewFields = "<viewFields>" + ViewFields + "</viewFields>";
+	};
+
+		// Setup the body of the request
+	var SoapBody = "";
+
+		// Set the body of the SOAP request
+	SoapBody = '<?xml version="1.0" encoding="utf-8"?>\
+	<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">\
+	<soap:Body>\
+	  <GetListItems xmlns="http://schemas.microsoft.com/sharepoint/soap/">\
+	    <listName>' + ListName + '</listName>\
+	    <viewName>' + ViewName + '</viewName>\
+	    ' + Query + '\
+	    ' + QueryOptions + '\
+	    ' + ViewFields + '\
+	  </GetListItems>\
+	</soap:Body>\
+	</soap:Envelope>';
+
+		// Add the Call to the passed request
+	Request.addCall("SOAP", ServiceURL, SoapBody, {"SOAPAction":"http://schemas.microsoft.com/sharepoint/soap/GetListItems"});
+
+};
+
+
+	// DP_SharePoint.callListService_Update Method
+	//
+DP_SharePoint.callListService_Update = function( Request, ServiceURL, ListName, Batch ) {
+
+		// Setup the body of the request
+	var SoapBody = "";
+
+	SoapBody = '<?xml version=\"1.0\" encoding=\"utf-8\"?>\
+	<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">\
+	<soap:Body>\
+		<UpdateListItems xmlns=\"http://schemas.microsoft.com/sharepoint/soap/\">\
+			<listName>' + ListName + '</listName>\
+			<updates>\
+				<Batch OnError=\"Continue\">' + Batch + '</Batch>\
+			</updates>\
+		</UpdateListItems>\
+	</soap:Body>\
+	</soap:Envelope>';
+
+		// Add the Call to the passed request
+	Request.addCall("SOAP", ServiceURL, SoapBody, {"SOAPAction":"http://schemas.microsoft.com/sharepoint/soap/UpdateListItems"});
+
+};
+
+
+	// DP_SharePoint.cleanSP_MultiLineText Method
+	//
+DP_SharePoint.cleanSP_MultiLineText = function( XML, ColName ) {
+
+		// Get Rows
+	var Rows = XML.getElementsByTagName("z:row");
+
+		// Loop over rows
+	for ( var Cnt=0; Cnt < Rows.length; Cnt++ ) {
+
+			// Get Current Value
+		var CurVal = "";
+		CurVal = Rows[Cnt].getAttribute(ColName);
+			// Create a Temporary Div to store the HTML
+		var TempDiv = document.createElement("div");
+		TempDiv.innerHTML = CurVal;
+			// Pull the (now parsed) value from the div
+		if ( TempDiv.innerText !== undefined ) {
+			Rows[Cnt].setAttribute(ColName, TempDiv.innerText); // IE
+		} else {
+			Rows[Cnt].setAttribute(ColName, TempDiv.textContent); // FF
+		};
+			// Null out the TempDiv
+		TempDiv = null;
+
+	};
+
+		// Return a Reference to the XML
+	return XML;
+
+};
+
+
+	// DP_SharePoint.cleanSP_UserList Method
+	//
+DP_SharePoint.cleanSP_UserList = function( XML, ColName ) {
+
+		// Get Rows
+	var Rows = XML.getElementsByTagName("z:row");
+
+		// Loop over rows
+	for ( var Cnt=0; Cnt < Rows.length; Cnt++ ) {
+
+			// Get Current Value
+		var CurVal = "";
+		CurVal = Rows[Cnt].getAttribute(ColName);
+
+			// Strip ID values
+		var TempVal = CurVal.replace(/[0-9]{1,5};#/g, "");
+			// Convert remaining separators to ", "
+		TempVal = TempVal.replace(/;#/g, ", ");
+
+			// Set new Value
+		Rows[Cnt].setAttribute(ColName, TempVal);
+
+	};
+
+		// Return a Reference to the XML
+	return XML;
+
+};
+
+
+	// DP_SharePoint.cleanSP_CalculatedField Method
+	//
+DP_SharePoint.cleanSP_CalculatedField = function( XML, ColName ) {
+
+		// Get Rows
+	var Rows = XML.getElementsByTagName("z:row");
+
+		// Loop over rows
+	for ( var Cnt=0; Cnt < Rows.length; Cnt++ ) {
+
+			// Get Current Value
+		var CurVal = "";
+		CurVal = Rows[Cnt].getAttribute(ColName);
+
+			// Split the value on the type identifier
+		var TempVal = CurVal.split(";#");
+
+			// Set new Value
+		Rows[Cnt].setAttribute(ColName, TempVal[1]);
+
+	};
+
+		// Return a Reference to the XML
+	return XML;
+
+};
+
+
+	// DP_SharePoint.cleanSP_LookupList Method
+	//
+DP_SharePoint.cleanSP_LookupList = function( XML, ColName, NoValuePlaceholder ) {
+
+		// Get Rows
+	var Rows = XML.getElementsByTagName("z:row");
+
+		// Loop over rows
+	for ( var Cnt=0; Cnt < Rows.length; Cnt++ ) {
+
+			// Get Current Value
+		var CurValue = "";
+		CurVal = Rows[Cnt].getAttribute(ColName);
+
+			// If the attribute exists, clean it up
+		if ( CurVal ) {
+				// Strip ID values
+			var TempVal = CurVal.replace(/[0-9]{1,5};#/g, "");
+				// Convert remaining separators to "; "
+			TempVal = TempVal.replace(/;#/g, "; ");
+				// If the first character is a separator, get rid of it.
+			TempVal = TempVal.replace(/; /, "");
+	
+				// Set new Value
+			Rows[Cnt].setAttribute(ColName, TempVal);
+		
+		} else {
+
+				// Set Value to NoValuePlaceholder
+			Rows[Cnt].setAttribute(ColName, NoValuePlaceholder);		
+		
+		};
+
+	};
+
+		// Return a Reference to the XML
+	return XML;
+
+};
+
+
+	// DP_SharePoint.cleanSP_DateTime Method
+	//
+DP_SharePoint.cleanSP_DateTime = function( XML, ColName, TimeFormat, DateFormat, NonDateValue ) {
+
+		// This method requires functions from DP_DateExtensions
+	if ( !Date.DP_DateExtensions ) {
+		throw new Error("The DP_SharePoint.cleanSP_DateTime() method requires that DP_DateExtensions (available from depressedpress.com) be loaded.");
+	};
+
+		// Get Rows
+	var Rows = XML.getElementsByTagName("z:row");
+
+		// Loop over rows
+	for ( var Cnt=0; Cnt < Rows.length; Cnt++ ) {
+
+			// Get Current Value
+		var CurVal = "";
+		CurVal = Rows[Cnt].getAttribute(ColName);
+
+			// Parse the Date
+		var CurDate = Date.parseFormat(CurVal, "YYYY-MM-DD HH:mm:ss");
+
+			// Determine what to 
+		var TempVal = "";
+		if ( CurDate != null ) {
+			if ( TimeFormat != null && DateFormat == null ) {
+				TempVal = CurDate.timeFormat(TimeFormat);
+			} else if ( TimeFormat == null && DateFormat != null ) {
+				TempVal = CurDate.dateFormat(DateFormat);
+			} else {
+				TempVal = CurDate.timeFormat(TimeFormat) + " " + CurDate.dateFormat(DateFormat);
+			};
+		} else {
+			if ( NonDateValue != null ) {
+				TempVal = NonDateValue;
+			} else {
+				TempVal = CurVal;
+			};
+		};
+
+			// Set new Value
+		Rows[Cnt].setAttribute(ColName, TempVal);
+
+	};
+
+		// Return a Reference to the XML
+	return XML;
+
+};
+
+
+	// DP_SharePoint.ReplaceLinks Method
+	//
+DP_SharePoint.ReplaceLinks = function() {
+
+		// Obtain a collection of all page links
+	var AllLinks = document.getElementsByTagName("a");
+	var CurMatch;
+		// Set the Target Proxy
+	var TargetProtocol = "news";
+	var ReplacementMask = new RegExp(TargetProtocol + "://\\*([A-Z0-9]{1,15})\\*", "i");
+
+		// Loop over all links and find those marked for replacement	
+	for( var cnt=0, limit = AllLinks.length; cnt < limit; ++cnt ) {
+		CurMatch = null;
+		if ( AllLinks[cnt].href ) {
+		if ( (CurMatch = AllLinks[cnt].href.match(ReplacementMask)) != null ) {
+			switch ( CurMatch[1] ) {
+				case "sametime":
+					ReplaceLinks_Sametime( AllLinks[cnt] );
+					break;
+				default:
+					ReplaceLinks_Other( AllLinks[cnt], CurMatch[1] );
+					break;
+
+			};
+		};
+		};
+ 	};
+
+	function ReplaceLinks_Sametime(CurLink) {
+
+			// If needed, add the Sametime style sheet to the head
+		if ( !document.getElementById("metReplaceLinks_SametimeStyle") ) {
+			var StyleElement = document.createElement("link");
+			StyleElement.setAttribute("id", "metReplaceLinks_SametimeStyle");
+			StyleElement.setAttribute("rel", "stylesheet");
+			StyleElement.setAttribute("href", "http://localhost:59449/stwebapi/main.css");
+			StyleElement.setAttribute("type", "text/css");
+			document.getElementsByTagName("head")[0].appendChild(StyleElement);
+		};
+			// If needed, add the Sametime script sheet to the head
+		if ( !document.getElementById("metReplaceLinks_SametimeScript") ) {
+			var ScriptElement = document.createElement("script");
+			ScriptElement.setAttribute("id", "metReplaceLinks_SametimeScript");
+			ScriptElement.setAttribute("type", "text/javascript");
+			ScriptElement.setAttribute("src", "http://localhost:59449/stwebapi/getStatus.js");
+			document.getElementsByTagName("head")[0].appendChild(ScriptElement);
+		};
+
+			// Determine the ID to use
+		var CurID = CurLink.href.replace(ReplacementMask, "");
+		CurID = CurID.replace("/", "");
+			// Get the Content of the link (the Sametime ID) and strip any crap HTML out of it
+		if ( CurID == "" ) {
+			var TempID = CurLink.innerHTML;
+			CurID = TempID.replace(/<.*?>/g, ''); 
+		};
+
+			// Modify the CurLink
+		CurLink.removeAttribute("href");
+		CurLink.setAttribute("class", "awareness");
+		CurLink.style.fontSize = "8pt";
+		CurLink.setAttribute("userId", CurID);
+
+	};
+
+	function ReplaceLinks_Other(CurLink, NewProtocol) {
+
+		CurLink.href = CurLink.href.replace(ReplacementMask, NewProtocol + ":\\\\");
+
+	};
+
+};
---
+++ README.md
@@ -0,0 +1,25 @@
+<h2>DP_QueryString</h2>
+
+<ul>	<li>Author: Jim Davis, the Depressed Press</li>
+	<li>Created: Sept 01, 2012</li>
+	<li><b>Documentation</b>: http://depressedpress.com/javascript-extensions/dp_sharepoint/</li>
+	<li>Contact: http://depressedpress.com/about/contact-me/</li>
+	<li>Other Components: http://depressedpress.com/javascript-extensions/</li>
+</ul>
+
+<p>The DP_SharePoint library provides tools to make working with Microsoft SharePoint simpler.  The library has been tested with Sharepoint 2007 and 2010 and, as it is a work in progress, should be considered BETA quality.  The library provides:</p>
+<ul><li>Methods to acquire JavaScript handlers to SharePoint form controls and fields.  This make it much simpler to interrogate or add event handlers to SharePoint forms.</li>
+	<li>Simple methods to add or remove events from SharePoint form elements.</li>
+	<li>Simple methods to hide and show elements of SharePoint forms.</li>
+	<li>Methods to simplify the calling and use of SharePoint web services and ready the data returned from them for use.</li>
+</ul>
+<p>The extensions are self-contained and present an extremely small footprint.  Only a single global object, "DP_SharePoint", is created.</p>
+<p>This component requires a JavaScript (ECMAScript) 1.3 (or better) development environment and has been tested successfully on Internet Explorer 8+.</p>
+
+<blockquote style="background: #dedede;">
+Copyright (c) 1996-2013, The Depressed Press (depressedpress.com)
+<br />
+All rights reserved.
+<br />
+Covered under the BSD Open Source License (included in the code).  Full legal information here: http://depressedpress.com/about/source-code-policy/
+</blockquote>
